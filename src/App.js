7bf6cd07eba76f51a00e62d00b5ce06febaf1962
okmasa <66762335+okmasa@users.noreply.github.com>
2021-10-03 12:01:18 +0900

    テンプレートとページの作成

--- src/App.js
+++ src/App.js
@@ -1,27 +1,6 @@
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
+import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "おくむら",
-  image: "https://source.unsplash.com/5PVXkqt2s9k",
-  email: "12345example.com",
-  phone: "123-4567-8910",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
-
 export default function App() {
-  return (
-    <div className="App">
-      <PrimaryButton>テスト</PrimaryButton>
-      <SecondaryButton>検索</SecondaryButton>
-      <SearchInput></SearchInput>
-      <UserCard user={user} />
-    </div>
-  );
+  return <Router />;
 }
